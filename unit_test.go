5b93dafadc43b457c72a55510d6c4f535cf4b8cb
zen <zhangyiming748@gmail.com>
2023-03-12 13:45:39 +0800

    去除文件名中重复的特定符号

--- unit_test.go
+++ unit_test.go
@@ -1,9 +1,33 @@
 package AVmerger
 
 import (
+	"encoding/json"
+	"os"
 	"testing"
 )
 
 func TestAllIn(t *testing.T) {
-	AllIn("/Users/zen/Github/AVmerger/file")
+	AllIn("/Volumes/swap/download")
+}
+
+func TestGet(t *testing.T) {
+	ret := get("/Volumes/swap/download")
+	file, err := os.OpenFile("list.json", os.O_APPEND|os.O_CREATE|os.O_RDWR, 0777)
+	if err != nil {
+		return
+	}
+	defer file.Close()
+	marshal, err := json.Marshal(ret)
+	if err != nil {
+		return
+	}
+	file.WriteString(string(marshal))
+
+}
+
+func TestDup(t *testing.T) {
+	s1 := "a__s___1_2"
+	// 期望 "a_s_1_2"
+	ret := duplicate(s1, '_')
+	t.Log(ret)
 }
