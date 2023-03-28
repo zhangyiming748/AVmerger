6bcd25cea68eef8b0369664d41bca1f6a0e3a8aa
zen <zhangyiming748@gmail.com>
2023-03-28 20:23:13 +0800

    fix

--- merge.go
+++ merge.go
@@ -118,7 +118,7 @@ func AllIn(root string) {
 	infos := get(root)
 	slog.Debug("解析后", slog.Any("返回的视频", infos))
 	for i, info := range *infos {
-		slog.Info(fmt.Sprint("正在合并第 %d/%d 个视频\n", i+1, len(*infos)))
+		slog.Info(fmt.Sprintf("正在合并第 %d/%d 个视频\n", i+1, len(*infos)))
 		Bash(root, info)
 	}
 }
