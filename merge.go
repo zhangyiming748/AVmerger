09c26868b6823ad06c2b531d2a27e0af60df7c88
zen <zhangyiming748@gmail.com>
2023-03-29 20:14:16 +0800

    fix

--- merge.go
+++ merge.go
@@ -229,15 +229,15 @@ func Bash(dst string, info Info) {
 	name := strings.Join([]string{n, "mp4"}, ".")
 	target := strings.Join([]string{dst, name}, string(os.PathSeparator))
 	cmd := exec.Command("ffmpeg", "-i", info.Video, "-i", info.Audio, target)
-	slog.Info("", slog.Any("生成的命令", fmt.Sprint(cmd)))
+	slog.Info("命令", slog.Any("ffmpeg", fmt.Sprint(cmd)))
 	stdout, err := cmd.StdoutPipe()
 	cmd.Stderr = cmd.Stdout
 	if err != nil {
-		slog.Warn("", slog.Any("cmd.StdoutPipe产生的错误", err))
+		slog.Warn("错误", slog.Any("cmd.StdoutPipe", err))
 		return
 	}
 	if err = cmd.Start(); err != nil {
-		slog.Warn("", slog.Any("cmd.Run产生的错误", err))
+		slog.Warn("错误", slog.Any("cmd.Run", err))
 		return
 	}
 	for {
@@ -251,13 +251,13 @@ func Bash(dst string, info Info) {
 		}
 	}
 	if err = cmd.Wait(); err != nil {
-		slog.Warn("", slog.Any("命令执行中有错误产生:%v\n", err))
+		slog.Warn("错误", slog.Any("命令执行中", err))
 		return
 	}
 	slog.Info("完成当前文件的处理", slog.Any("源文件", info.Name), slog.Any("目标文件夹", dst))
 	if err := os.RemoveAll(info.Del); err != nil {
 		slog.Warn("", slog.Any("删除源文件失败", err))
 	} else {
-		slog.Warn("", slog.Any("删除源目录:%v\n", info.Del))
+		slog.Warn("", slog.Any("删除源目录", info.Del))
 	}
 }
