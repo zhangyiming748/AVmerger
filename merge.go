375ceac69bf864d363cb440b0a6c297b779b95c7
zen <zhangyiming748@gmail.com>
2023-05-31 17:49:04 +0800

    fix

--- merge.go
+++ merge.go
@@ -81,7 +81,7 @@ func AllInH265(root string) {
 	infos := get(root)
 	slog.Debug("解析后", slog.Any("返回的视频", infos))
 	for i, info := range *infos {
-		slog.Info(fmt.Sprintf("正在合并第 %d/%d 个视频\n", i+1, len(*infos)))
+		slog.Info(fmt.Sprintf("正在合并第 %d/%d 个视频", i+1, len(*infos)))
 		avc(root, info)
 	}
 }
