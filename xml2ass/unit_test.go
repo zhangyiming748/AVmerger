baa747cbb7dced0ef50dc432262bc0395691680f
zen <zhangyiming748@gmail.com>
2024-02-29 19:47:02 +0800

    单独测试转换ass命令

--- xml2ass/unit_test.go
+++ xml2ass/unit_test.go
@@ -1,9 +1,15 @@
 package xml2ass
 
 import (
+	"github.com/zhangyiming748/AVmerger/constant"
+	"github.com/zhangyiming748/GetFileInfo"
 	"testing"
 )
 
+//go test -v -run TestConv
 func TestConv(t *testing.T) {
-	Conv("file")
+	xmls := GetFileInfo.GetAllFileInfo(constant.ANDROIDDANMAKU, "xml")
+	for _, xml := range xmls {
+		Conv(xml)
+	}
 }
--- xml2ass/xml2ass.go
+++ xml2ass/xml2ass.go
@@ -2,6 +2,7 @@ package xml2ass
 
 import (
 	"fmt"
+	"github.com/zhangyiming748/GetFileInfo"
 	"log/slog"
 	"os"
 	"os/exec"
@@ -11,17 +12,18 @@ import (
 /*
 转换弹幕文件到原始位置
 */
-func Conv(xml string) (string, error) {
+func Conv(xml GetFileInfo.BasicInfo) (string, error) {
 	// danmaku2ass danmaku.xml -s 1280x720 -dm 15 -fs 45 -a 50 -o danmaku.ass
-	danmaku := strings.Replace(xml, ".xml", ".ass", 1)
-	output, err := exec.Command("danmaku2ass", xml, "-s", "1280x720", "-dm", "15", "-fs", "45", "-a", "50", "-r", "-o", danmaku).CombinedOutput()
-	slog.Debug("生成命令", slog.String("命令原文", fmt.Sprint(danmaku)))
+	ass := strings.Replace(xml.FullPath, ".xml", ".ass", 1)
+	exe := exec.Command("danmaku2ass", xml.FullPath, "-s", "1280x720", "-dm", "15", "-fs", "45", "-a", "50", "-r", "-o", ass)
+	output, err := exe.CombinedOutput()
+	slog.Debug("生成命令", slog.String("命令原文", fmt.Sprint(exe)))
 	if err != nil {
 		slog.Warn("当前弹幕文件转换错误", slog.Any("文件信息", xml), slog.Any("错误原文", err))
 		return "", err
 	} else {
 		slog.Info("当前弹幕文件转换成功", slog.Any("命令输出", string(output)))
-		return danmaku, nil
+		return ass, nil
 	}
 }
 func isExist(dir string) bool {
