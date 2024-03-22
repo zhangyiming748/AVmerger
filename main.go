a95c7e112f348c3e74d2d323bcedd51d122dc5d1
zen <zhangyiming748@gmail.com>
2024-03-22 19:52:47 +0800

    防止重复查找

--- main.go
+++ main.go
@@ -18,23 +18,28 @@ func init() {
 }
 
 func main() {
+	found := false
 	if merge.IsExist(constant.BILI) {
 		merge.Merge(constant.BILI)
+		found = true
 	}
 	if merge.IsExist(constant.HD) {
 		merge.Merge(constant.HD)
+		found = true
 	}
 	if merge.IsExist(constant.GLOBAL) {
 		merge.Merge(constant.GLOBAL)
+		found = true
 	}
 	src := strings.Join([]string{getRoot(), "download"}, string(os.PathSeparator))
 	if merge.IsExist(src) {
-		merge.Merge(src)
+		if !found {
+			merge.Merge(src)
+		}
 	}
 }
 
 func getRoot() string {
 	_, filename, _, _ := runtime.Caller(0)
-
 	return path.Dir(filename)
 }
