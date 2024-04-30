e5466813812c94ac5e2bf05bc08fc251ee290892
zen <zhangyiming748@gmail.com>
2024-04-30 10:07:13 +0800

    只有当源文件比目标文件大的时候才删除

--- compress/compress.go
+++ compress/compress.go
@@ -5,6 +5,7 @@ import (
 	"github.com/zhangyiming748/AVmerger/constant"
 	"github.com/zhangyiming748/AVmerger/util"
 	"log/slog"
+	"os"
 	"os/exec"
 	"strings"
 )
@@ -20,7 +21,46 @@ func main() {
 		output, err := cmd.CombinedOutput()
 		if err != nil {
 			return
+		} else {
+			if compere(file.FullPath, after) == "smaller" {
+				err := os.Remove(file.FullPath)
+				if err != nil {
+					return
+				} else {
+					slog.Warn(fmt.Sprintf("删除文件%v\n", file.FullPath))
+				}
+			} else {
+				slog.Warn("转换后的文件比源文件更大", slog.String("源文件", file.FullPath), slog.String("目标文件", after), slog.String("命令原文", cmd.String()))
+			}
+
 		}
 		slog.Debug(fmt.Sprintln("命令输出", string(output)))
 	}
+}
+func compere(before, after string) (result string) {
+	file1 := before
+	file2 := after
+
+	info1, err1 := os.Stat(file1)
+	info2, err2 := os.Stat(file2)
+
+	if err1 != nil || err2 != nil {
+		fmt.Println("获取文件信息失败：", err1, err2)
+		return
+	}
+
+	size1 := info1.Size()
+	size2 := info2.Size()
+
+	if size1 > size2 {
+		fmt.Printf("%s 文件大小为 %d 字节，大于 %s 文件大小 %d 字节", file1, size1, file2, size2)
+		result = "smaller"
+	} else if size1 < size2 {
+		fmt.Printf("%s 文件大小为 %d 字节，小于 %s 文件大小 %d 字节", file1, size1, file2, size2)
+		result = "bigger"
+	} else {
+		fmt.Printf("%s 文件大小为 %d 字节，等于 %s 文件大小 %d 字节", file1, size1, file2, size2)
+		result = "equal"
+	}
+	return result
 }
