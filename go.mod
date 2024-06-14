40b21e936c4dcdfefa9105e2d72719e520dfcec3
zen <zhangyiming748@gmail.com>
2024-06-14 16:45:39 +0800

    记录分页日志

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/zhangyiming748/AVmerger
 
-go 1.21
+go 1.22
+
+toolchain go1.22.4
 
 require (
 	gorm.io/driver/sqlite v1.5.5
@@ -11,4 +13,5 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/mattn/go-sqlite3 v1.14.22 // indirect
+	github.com/zhangyiming748/lumberjack v0.0.1 // indirect
 )
--- main.go
+++ main.go
@@ -1,16 +1,23 @@
 package main
 
 import (
+	"fmt"
 	"github.com/zhangyiming748/AVmerger/constant"
 	"github.com/zhangyiming748/AVmerger/merge"
 	"github.com/zhangyiming748/AVmerger/util"
+	"github.com/zhangyiming748/lumberjack"
+	"io"
 	"log"
 	"os"
 	"path"
 	"runtime"
 	"strings"
+	"time"
 )
 
+func init() {
+	setLog()
+}
 func main() {
 	defer func() {
 		err := os.Chmod("merge.db", 0666)
@@ -46,4 +53,36 @@ func getRoot() string {
 	log.Printf("当前的工作目录:%v\n", filename)
 	util.SetRoot(path.Dir(filename))
 	return util.GetRoot()
+}
+
+func setLog() {
+	// 创建一个用于写入文件的Logger实例
+	fileLogger := &lumberjack.Logger{
+		Filename:   "/data/data/com.termux/files/home/storage/movies/bili/AVmerge.log",
+		MaxSize:    1, // MB
+		MaxBackups: 3,
+		MaxAge:     28, // days
+	}
+
+	// 创建一个用于输出到控制台的Logger实例
+	consoleLogger := log.New(os.Stdout, "CONSOLE: ", log.LstdFlags)
+
+	// 设置文件Logger
+	//log.SetOutput(fileLogger)
+
+	// 同时输出到文件和控制台
+	log.SetOutput(io.MultiWriter(fileLogger, consoleLogger.Writer()))
+	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	// 在这里开始记录日志
+
+	// 记录更多日志...
+
+	// 关闭日志文件
+	//defer fileLogger.Close()
+}
+func NumsOfGoroutine() {
+	for {
+		fmt.Printf("当前程序运行时协程个数:%d\n", runtime.NumGoroutine())
+		time.Sleep(1 * time.Second)
+	}
 }
