af4764f8236583929cc2620e551e65d04ecfc098
zen <hasee>
2025-01-23 15:44:43 +0800

    rsync放在util目录

--- main.go
+++ main.go
@@ -5,7 +5,6 @@ import (
 	"io"
 	"log"
 	"os"
-	"os/exec"
 	"path"
 	"path/filepath"
 	"runtime"
@@ -41,8 +40,8 @@ func main() {
 				log.Printf("程序正确执行但删除文件夹失败:%v\n", err)
 			} else {
 				log.Printf("程序正确执行,删除文件夹:%v\n", constant.BILI)
-				err1 := UploadWithRsync(constant.ANDROIDVIDEO)
-				err2 := UploadWithRsync(constant.ANDROIDAUDIO)
+				err1 := util.UploadWithRsync(constant.ANDROIDVIDEO)
+				err2 := util.UploadWithRsync(constant.ANDROIDAUDIO)
 				if err1 != nil || err2 != nil {
 					log.Printf("上传视频文件夹失败:%v\n", err1)
 					log.Printf("上传音频文件夹失败:%v\n", err2)
@@ -63,8 +62,8 @@ func main() {
 				log.Printf("程序正确执行但删除文件夹失败:%v\n", err)
 			} else {
 				log.Printf("程序正确执行,删除文件夹:%v\n", constant.HD)
-				err1 := UploadWithRsync(constant.ANDROIDVIDEO)
-				err2 := UploadWithRsync(constant.ANDROIDAUDIO)
+				err1 := util.UploadWithRsync(constant.ANDROIDVIDEO)
+				err2 := util.UploadWithRsync(constant.ANDROIDAUDIO)
 				if err1 != nil || err2 != nil {
 					log.Printf("上传视频文件夹失败:%v\n", err1)
 					log.Printf("上传音频文件夹失败:%v\n", err2)
@@ -85,8 +84,8 @@ func main() {
 				log.Printf("程序正确执行但删除文件夹失败:%v\n", err)
 			} else {
 				log.Printf("程序正确执行,删除文件夹:%v\n", constant.GLOBAL)
-				err1 := UploadWithRsync(constant.ANDROIDVIDEO)
-				err2 := UploadWithRsync(constant.ANDROIDAUDIO)
+				err1 := util.UploadWithRsync(constant.ANDROIDVIDEO)
+				err2 := util.UploadWithRsync(constant.ANDROIDAUDIO)
 				if err1 != nil || err2 != nil {
 					log.Printf("上传视频文件夹失败:%v\n", err1)
 					log.Printf("上传音频文件夹失败:%v\n", err2)
@@ -107,8 +106,8 @@ func main() {
 				log.Printf("程序正确执行但删除文件夹失败:%v\n", err)
 			} else {
 				log.Printf("程序正确执行,删除文件夹:%v\n", constant.BLUE)
-				err1 := UploadWithRsync(constant.ANDROIDVIDEO)
-				err2 := UploadWithRsync(constant.ANDROIDAUDIO)
+				err1 := util.UploadWithRsync(constant.ANDROIDVIDEO)
+				err2 := util.UploadWithRsync(constant.ANDROIDAUDIO)
 				if err1 != nil || err2 != nil {
 					log.Printf("上传视频文件夹失败:%v\n", err1)
 					log.Printf("上传音频文件夹失败:%v\n", err2)
@@ -211,31 +210,4 @@ func isExist(path string) bool {
 		fmt.Println("发生错误：", err)
 		return false
 	}
-}
-
-func UploadWithRsync(localDir string) error {
-	remoteDir := "/home/zen/ugreen/alist/bili/" // 服务器上的目标目录，请根据实际情况修改
-	user := "zen"                               // 服务器用户名，请根据实际情况修改
-	server := "192.168.1.9"
-	password := "123456"
-	// 强烈建议不要在代码中直接使用密码，这里只是为了演示
-	// 实际应用中应该从环境变量或配置文件读取密码
-	// password := os.Getenv("RSYNC_PASSWORD")
-
-	cmdStr := fmt.Sprintf("sshpass -p '%s' rsync -vzrc --progress %s %s@%s:%s", password, localDir, user, server, remoteDir)
-	cmd := exec.Command("bash", "-c", cmdStr)
-
-	log.Printf("执行的 rsync 命令是: %s\n", cmd.String())
-
-	// 将命令的标准输出和标准错误输出连接到当前进程，以便实时查看进度
-	cmd.Stdout = os.Stdout
-	cmd.Stderr = os.Stderr
-
-	err := cmd.Run()
-	if err != nil {
-		return fmt.Errorf("rsync 上传失败: %v", err)
-	}
-
-	log.Printf("成功上传文件夹 %s 到 %s@%s:%s\n", localDir, user, server, remoteDir)
-	return nil
 }
--- util/rsync.go
+++ util/rsync.go
@@ -5,15 +5,31 @@ import (
 	"log"
 	"os"
 	"os/exec"
-	"strings"
 )
 
-func RsyncDir(localDir, remoteDir, user, server, password string) error {
-	// 使用sshpass来传递密码并构建rsync命令
-	args := []string{"sshpass", "-p", password, "rsync", "-vz", localDir, fmt.Sprintf("%s@%s:%s", user, server, remoteDir)}
-	cmd := exec.Command("bash", "-c", strings.Join(args, " "))
-	log.Printf("最终的rsync命令是:%s\n", cmd.String())
+func UploadWithRsync(localDir string) error {
+	remoteDir := "/home/zen/ugreen/alist/bili/" // 服务器上的目标目录，请根据实际情况修改
+	user := "zen"                               // 服务器用户名，请根据实际情况修改
+	server := "192.168.1.9"
+	password := "123456"
+	// 强烈建议不要在代码中直接使用密码，这里只是为了演示
+	// 实际应用中应该从环境变量或配置文件读取密码
+	// password := os.Getenv("RSYNC_PASSWORD")
+
+	cmdStr := fmt.Sprintf("sshpass -p '%s' rsync -vzrc --progress %s %s@%s:%s", password, localDir, user, server, remoteDir)
+	cmd := exec.Command("bash", "-c", cmdStr)
+
+	log.Printf("执行的 rsync 命令是: %s\n", cmd.String())
+
+	// 将命令的标准输出和标准错误输出连接到当前进程，以便实时查看进度
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	return cmd.Run()
+
+	err := cmd.Run()
+	if err != nil {
+		return fmt.Errorf("rsync 上传失败: %v", err)
+	}
+
+	log.Printf("成功上传文件夹 %s 到 %s@%s:%s\n", localDir, user, server, remoteDir)
+	return nil
 }
