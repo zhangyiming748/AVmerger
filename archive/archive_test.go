166dba11bc906f1a028d668ebeb98d0750c0b519
zen <mbp>
2025-03-19 22:53:23 +0800

    test(archive): 在测试中添加清屏功能以提升可读性
    在测试执行过程中，添加清屏命令以清除终端屏幕，避免输出信息过于混乱，提升测试结果的可读性

--- archive/archive_test.go
+++ archive/archive_test.go
@@ -10,8 +10,12 @@ func init() {
 }
 //go test -timeout 1000h -v -run TestAll
 func TestAll(t *testing.T) {
-	files,_:=GetAllFiles("/Users/zen/Movies")
-	for _,file:=range files {
+	files, _ := GetAllFiles("/Users/zen/Movies")
+	for _, file := range files {
+		// 清空终端屏幕
+		print("\033[H\033[2J")
+		// 或者使用这个命令也可以
+		// fmt.Print("\033[H\033[2J\033[3J")
 		Convert(file)
 	}
 }
