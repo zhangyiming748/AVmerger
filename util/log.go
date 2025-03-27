886c68b12cc50e7fd7f0e096d4bb7219aa5e7fce
zen <mbp>
2025-03-28 03:06:22 +0800

    refactor(log): 修改日志输出格式以仅显示时间
    将日志输出格式从显示日期和时间改为仅显示时间，以提高日志的可读性并减少冗余信息

--- util/log.go
+++ util/log.go
@@ -55,7 +55,7 @@ func SetLog() {
 
 	// 同时输出到文件和控制台
 	log.SetOutput(io.MultiWriter(fileLogger, consoleLogger.Writer()))
-	log.SetFlags(log.LstdFlags | log.Lshortfile)
+	log.SetFlags(log.Ltime | log.Lshortfile)
 	// 在这里开始记录日志
 
 	// 记录更多日志...
