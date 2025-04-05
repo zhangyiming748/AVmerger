fcfe1428ddb91b8409d2d073772bd7f468fbc351
zen <mbp>
2025-04-06 00:39:10 +0800

    refactor(unit_test.go): 调整导入顺序并添加日志设置
    删除旧的LICENSE文件
    ```
    
    ### 解释:
    1. **refactor(unit_test.go)**: 对 `unit_test.go` 文件进行了代码重构，调整了导入顺序并添加了日志设置。
    2. **删除旧的LICENSE文件**: 删除了旧的 `LICENSE` 文件，这可能是因为文件内容已过时或不再适用。

--- archive/unit_test.go
+++ archive/unit_test.go
@@ -1,11 +1,15 @@
 package archive
 
 import (
-	"github.com/zhangyiming748/archiveVideos"
 	"testing"
+
+	"github.com/zhangyiming748/AVmerger/util"
+	"github.com/zhangyiming748/archiveVideos"
 )
+
 // go test -v -timeout 30h -run TestArchiveByGoModule
 func TestArchiveByGoModule(t *testing.T) {
+	util.SetLog()
 	root := "/Users/zen/Movies"
 	archiveVideos.ArchiveVideos(root)
 }
