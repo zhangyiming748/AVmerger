317fdcb7dfe741e9df569a08975a5e1709a68146
zen <mbp>
2025-04-20 20:03:13 +0800

    refactor(constant): 更新Android视频和音频存储路径
    将ANDROIDVIDEO和ANDROIDAUDIO的路径分别更新为"/sdcard/Movies/bili_video"和"/sdcard/Music/bili_audio"，以便更好地组织和管理Bilibili下载的视频和音频文件。

--- constant/parameter.go
+++ constant/parameter.go
@@ -5,8 +5,8 @@ const (
 	HD           = "/sdcard/Android/data/tv.danmaku.bilibilihd/download"
 	GLOBAL       = "/sdcard/Android/data/com.bilibili.app.in/download"
 	BLUE         = "/sdcard/Android/data/com.bilibili.app.blue/download"
-	ANDROIDVIDEO = "/sdcard/Movies"
-	ANDROIDAUDIO = "/sdcard/Music"
+	ANDROIDVIDEO = "/sdcard/Movies/bili_video"
+	ANDROIDAUDIO = "/sdcard/Music/bili_audio"
 	REMOTEVIDEO  = "/home/zen/ugreen/alist/bili/video"
 	REMOTEAUDIO  = "/home/zen/ugreen/alist/bili/audio"
 )
